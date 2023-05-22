5e758de40bdfdb744e6929be56d961e8c571ddd0
aag011 <114713082+aag011@users.noreply.github.com>
2023-05-22 13:17:07 -0700

    Update reddit_spider.py

--- reddit_scraper/spiders/reddit_spider.py
+++ reddit_scraper/spiders/reddit_spider.py
@@ -4,28 +4,26 @@ TOTAL_COUNT = 5000
 
 class RedditSpider(scrapy.Spider):
     name = "reddit_spider"
-    start_urls = ['https://www.reddit.com/r/popular/']
+    start_urls = ['https://www.reddit.com/r/AskReddit/']
     post_count = 0
 
     def parse(self, response):
-        # Extracting data from Reddit posts
-        for post in response.css('div.Post'):
-            title = post.css('h3._eYtD2XCVieq6emjKBH3m::text').get()
-            score = post.css('div._1rZYMD_4xY3gRcSS3p8ODO::text').get()
-            comments = post.css('span.FHCV02u6Cp2zYL0fhQPsO::text').get()
+        # Extracting question-answer data from Reddit threads
+        for thread in response.css('div.Post'):
+            question = thread.css('h3._eYtD2XCVieq6emjKBH3m::text').get()
+            answers = thread.css('div._1rZYMD_4xY3gRcSS3p8ODO::text').getall()
 
             yield {
-                'title': title,
-                'score': score,
-                'comments': comments
+                'question': question,
+                'answers': answers
             }
-
+            
             self.post_count += 1  # Increment the post count
 
-            if self.post_count >= TOTAL_COUNT:  # Stop scraping when post count reaches 5000
+            if self.post_count >= TOTAL_COUNT:  # Stop scraping when post count reaches TOTAL_COUNT
                 break
 
         # Follow the 'Next' button recursively
         next_page = response.css('a[rel="next"]::attr(href)').get()
-        if next_page is not None  and self.post_count < TOTAL_COUNT:
+        if next_page is not None and self.post_count < TOTAL_COUNT:
             yield response.follow(next_page, self.parse)
